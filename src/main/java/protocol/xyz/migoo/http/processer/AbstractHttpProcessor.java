2f42f8cba40b0e0f262d7a8111e8ed499f140473
xiaomi <mi_xiao@qq.com>
2021-12-12 16:47:29 +0800

    feat: 优化代码

--- src/main/java/protocol/xyz/migoo/http/processer/AbstractHttpProcessor.java
+++ src/main/java/protocol/xyz/migoo/http/processer/AbstractHttpProcessor.java
@@ -1,34 +1,34 @@
 /*
+ * The MIT License (MIT)
  *
- *  * The MIT License (MIT)
- *  *
- *  * Copyright (c) 2018. Lorem XiaoMiSum (mi_xiao@qq.com)
- *  *
- *  * Permission is hereby granted, free of charge, to any person obtaining
- *  * a copy of this software and associated documentation files (the
- *  * 'Software'), to deal in the Software without restriction, including
- *  * without limitation the rights to use, copy, modify, merge, publish,
- *  * distribute, sublicense, and/or sell copies of the Software, and to
- *  * permit persons to whom the Software is furnished to do so, subject to
- *  * the following conditions:
- *  *
- *  * The above copyright notice and this permission notice shall be
- *  * included in all copies or substantial portions of the Software.
- *  *
- *  * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
- *  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- *  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2021.  Lorem XiaoMiSum (mi_xiao@qq.com)
  *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * 'Software'), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package protocol.xyz.migoo.http.processer;
 
+import core.xyz.migoo.samplers.SampleResult;
 import core.xyz.migoo.testelement.TestStateListener;
 import protocol.xyz.migoo.http.AbstractHttpTestElement;
+import protocol.xyz.migoo.http.sampler.HTTPSampleResult;
 
 /**
  * @author xiaomi
@@ -43,5 +43,14 @@ public abstract class AbstractHttpProcessor extends AbstractHttpTestElement impl
     @Override
     public void testEnded() {
 
+    }
+
+    public SampleResult process() {
+        HTTPSampleResult result = new HTTPSampleResult(getPropertyAsString(TITLE));
+        try {
+            return super.execute(result);
+        } catch (Exception e) {
+            throw new RuntimeException(getClass().getSimpleName() + " error.", e);
+        }
     }
 }
--- src/main/java/protocol/xyz/migoo/jdbc/processor/AbstractJDBCProcessor.java
+++ src/main/java/protocol/xyz/migoo/jdbc/processor/AbstractJDBCProcessor.java
@@ -1,28 +1,26 @@
 /*
+ * The MIT License (MIT)
  *
- *  * The MIT License (MIT)
- *  *
- *  * Copyright (c) 2018. Lorem XiaoMiSum (mi_xiao@qq.com)
- *  *
- *  * Permission is hereby granted, free of charge, to any person obtaining
- *  * a copy of this software and associated documentation files (the
- *  * 'Software'), to deal in the Software without restriction, including
- *  * without limitation the rights to use, copy, modify, merge, publish,
- *  * distribute, sublicense, and/or sell copies of the Software, and to
- *  * permit persons to whom the Software is furnished to do so, subject to
- *  * the following conditions:
- *  *
- *  * The above copyright notice and this permission notice shall be
- *  * included in all copies or substantial portions of the Software.
- *  *
- *  * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
- *  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *  * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- *  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- *  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (c) 2021.  Lorem XiaoMiSum (mi_xiao@qq.com)
  *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * 'Software'), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package protocol.xyz.migoo.jdbc.processor;
@@ -47,13 +45,12 @@ public abstract class AbstractJDBCProcessor extends AbstractJDBCTestElement {
         }
         // 2. 从变量中取出保存的连接池
         DataSourceElement dataSource = (DataSourceElement) getVariables().get(dataSourceName);
-
         try (Connection conn = dataSource.getConnection()) {
             SampleResult result = execute(conn);
             result.setUrl(dataSource.getUrl());
             return result;
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(getClass().getSimpleName() + " error.", e);
         }
     }
 }
