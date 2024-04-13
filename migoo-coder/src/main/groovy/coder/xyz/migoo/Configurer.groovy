03bc8c9e97f451c86d42152f8eb36c934ae4f2ba
xiaomi <mi_xiao@qq.com>
2024-04-13 09:52:53 +0800

    :sparkles: upgrade coder Configurer.java -> Configurer.groovy

---
+++ migoo-coder/src/main/groovy/coder/xyz/migoo/Configurer.groovy
@@ -0,0 +1,23 @@
+package coder.xyz.migoo
+
+import coder.xyz.migoo.protocol.Protocol
+
+class Configurer extends El {
+
+    Configurer(String testClass, El protocol) {
+        super(testClass)
+        p(protocol.customize())
+    }
+
+    static Configurer withHttpDefaults(Protocol.HTTP config) {
+        return new Configurer("HttpDefaults", config)
+    }
+
+    static Configurer withJDBCDatasource(Protocol.JDBC config) {
+        return new Configurer("JDBCDatasource", config)
+    }
+
+    static Configurer withRedisDatasource(Protocol.Redis config) {
+        return new Configurer("RedisDatasource", config)
+    }
+}
