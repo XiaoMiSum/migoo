64a8bae4e7003279c1428f08036020486058a184
xiaomi <mi_xiao@qq.com>
2024-05-01 13:03:04 +0800

    :sparkles: upgrade coder Extractor.java -> Extractor.groovy

---
+++ migoo-coder/src/main/groovy/coder/xyz/migoo/Extractor.groovy
@@ -0,0 +1,23 @@
+package coder.xyz.migoo
+
+class Extractor extends El {
+
+    Extractor(String testClass, String referenceName, String field = null, Integer matchNo = null) {
+        super(testClass)
+        p("variable_name", referenceName)
+        p("field", field)
+        p("match_num", matchNo)
+    }
+
+    static Extractor withJSONExtractor(String referenceName, String jsonPath, Integer matchNo = null) {
+        return new Extractor("JSONExtractor", referenceName, jsonPath, matchNo)
+    }
+
+    static Extractor withRegexExtractor(String referenceName, String regex, Integer matchNo = null) {
+        return new Extractor("RegexExtractor", referenceName, regex, matchNo)
+    }
+
+    static Extractor withResultExtractor(String referenceName) {
+        return new Extractor("ResultExtractor", referenceName)
+    }
+}
