23cdf19d727bcd0407a7523e56f9f87652b88821
xiaomi <mi_xiao@qq.com>
2024-06-27 19:23:14 +0800

    :sparkles: 扩展函数支持keyword参数

--- migoo-core/src/main/java/function/xyz/migoo/Digest.java
+++ migoo-core/src/main/java/function/xyz/migoo/Digest.java
@@ -27,7 +27,10 @@ package function.xyz.migoo;
 
 import core.xyz.migoo.function.Args;
 import core.xyz.migoo.function.Function;
+import core.xyz.migoo.function.KwArgs;
+import core.xyz.migoo.function.LsArgs;
 import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang3.StringUtils;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -38,34 +41,55 @@ import java.security.NoSuchAlgorithmException;
  */
 public class Digest implements Function {
 
+
     /**
      * 获取信息摘要，通常为MD5，支持四个参数
-     * 参数顺序：
+     * 参数：
      * algorithm: 算法，允许为空，默认为md5
      * content: 待编码的原始内容，非空
      * salt：盐，允许为空
      * upper: 是否将结果转为大写，允许为空，默认 false
      */
     @Override
-    public Object execute(Args args) {
+    public String execute(Args args) {
         if (args.isEmpty()) {
             throw new IllegalArgumentException("parameters con not be null");
         }
+        return args instanceof KwArgs kwArgs ? execute(kwArgs) : execute((LsArgs) args);
+    }
+
+    private String execute(KwArgs args) {
+        var algorithm = args.getString("algorithm");
+        var content = args.getString("content");
+        var salt = args.getString("salt");
+        var upper = args.getBooleanValue("upper");
+        return hex(algorithm, content, salt, upper);
+    }
+
+    private String execute(LsArgs args) {
         if (args.size() == 1) {
             args.addFirst("md5");
         }
-        var algorithm = args.getString(0).isEmpty() ? "md5" : args.getString(0).trim();
-        if (args.getString(1).isEmpty()) {
+        var algorithm = args.getString(0).trim();
+        var content = args.getString(1);
+        var salt = args.getString(2);
+        var upper = args.getBooleanValue(3);
+        return hex(algorithm, content, salt, upper);
+    }
+
+    private String hex(String algorithm, String content, String salt, boolean upper) {
+        if (StringUtils.isBlank(content)) {
             throw new IllegalArgumentException("content is null or empty");
         }
+        algorithm = StringUtils.isBlank(algorithm) ? "md5" : algorithm.trim();
         try {
             var md = MessageDigest.getInstance(algorithm);
-            md.update(args.getString(1).getBytes(StandardCharsets.UTF_8));
-            if (!args.getString(2).isEmpty()) {
-                md.update(args.getString(2).getBytes(StandardCharsets.UTF_8));
+            md.update(content.getBytes(StandardCharsets.UTF_8));
+            if (StringUtils.isNotBlank(salt)) {
+                md.update(salt.getBytes(StandardCharsets.UTF_8));
             }
             byte[] bytes = md.digest();
-            return args.getBooleanValue(3) ? new String(Hex.encodeHex(bytes)).toUpperCase() : new String(Hex.encodeHex(bytes));
+            return upper ? new String(Hex.encodeHex(bytes)).toUpperCase() : new String(Hex.encodeHex(bytes));
         } catch (NoSuchAlgorithmException e) {
             throw new IllegalArgumentException(e.getMessage(), e);
         }
