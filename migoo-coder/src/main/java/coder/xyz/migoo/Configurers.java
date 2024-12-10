fa445ca0f013a949dd9330ece29f9c167fea4167
xiaomi <mi_xiao@qq.com>
2024-12-10 14:35:33 +0800

    :sparkles: 实现 rabbitmq 组件

--- migoo-coder/src/main/java/coder/xyz/migoo/Configurers.java
+++ migoo-coder/src/main/java/coder/xyz/migoo/Configurers.java
@@ -28,4 +28,8 @@ public class Configurers extends El {
     public static Configurers defaults(Kafka config) {
         return new Configurers("Kafka_Defaults", config);
     }
+
+    public static Configurers defaults(RabbitMQ config) {
+        return new Configurers("rabbitmq_defaults", config);
+    }
 }
